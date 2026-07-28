2aca2765f43968cac9f56e4ea915859961afd9d3
ivbash <va-n-ja@yandex.ru>
2026-07-28 15:36:20 +0500

    feat(web): tag id type

--- apps/web/src/entities/tag/index.ts
+++ apps/web/src/entities/tag/index.ts
@@ -5,4 +5,4 @@ export { tagKeys } from './model/tag-keys';
 export { useTag } from './model/use-tag';
 export { useTags } from './model/use-tags';
 export { TagNameSchema } from './model/schema';
-export type { Tag } from './model/types';
+export type * from './model/types';
--- apps/web/src/entities/tag/model/use-tag.ts
+++ apps/web/src/entities/tag/model/use-tag.ts
@@ -2,8 +2,9 @@ import { useQuery } from '@tanstack/react-query';
 import { tagApi } from '../api/tag-api';
 import { mapTagDto } from '../lib/map-tag-dto';
 import { tagKeys } from './tag-keys';
+import type { TagId } from './types';
 
-export function useTag(id: number) {
+export function useTag(id: TagId) {
   return useQuery({
     queryKey: tagKeys.detail(id),
     queryFn: async () => {
